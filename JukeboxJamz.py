8c0c4e26f5f33f278f9bb0dd23f3b60845b3fdb7
MrBallinz <68559009+MrBallinz@users.noreply.github.com>
2020-08-05 08:11:16 -0700

    Update JukeboxJamz.py

--- JukeboxJamz.py
+++ JukeboxJamz.py
@@ -82,50 +82,58 @@ DOWN = 'down', 'south'
 LEFT = 'left', 'west'
 RIGHT = 'right', 'east'
 
-solved_places = {'a1': False, 'a2': False, 'a3': False, 'a4': False,
-                'b1': False, 'b2': False, 'b3': False, 'b4': False,
-                'c1': False, 'c2': False, 'c3': False, 'c4': False,
-                'd1': False, 'd2': False, 'd3': False, 'd4': False,
-                 }
-zonemap = {
-    'a1': {
-        ZONENAME: "At the Jukebox",
-        DESCRIPTION = 'Your at the classic jukebox filled with some of the most classic hits'
-        EXAMINATION = 'You see a coin slot. it reads "10 cents per song" you see a forward and back button to select song'
-        SOLVED = False
-        UP = 'up', 'north'
-        DOWN = 'b1'
-        LEFT = 'left', 'west'
-        RIGHT = 'a2'
-    },
-        'a2': {
-        ZONENAME: "At the Booth",
-        DESCRIPTION = 'description'
-        EXAMINATION = 'examine'
-        SOLVED = False
-        UP = 'up', 'north'
-        DOWN = 'down', 'south'
-        LEFT = 'left', 'west'
-        RIGHT = 'right', 'east'
-    },
-        'b1': {
-        ZONENAME: "Inside the Diner",
-        DESCRIPTION = 'This is the Jukebox diner'
-        EXAMINATION = 'examine'
-        SOLVED = False
-        UP = 'up', 'north'
-        DOWN = 'down', 'south'
-        LEFT = 'left', 'west'
-        RIGHT = 'right', 'east'
-    },
-        'b2': {
-        ZONENAME: "Outside the Jukebox Diner",
-        DESCRIPTION = 'description'
-        EXAMINATION = 'examine'
-        SOLVED = False
-        UP = 'a2'
-        DOWN = ''
-        LEFT = 'b1'
-        RIGHT = 'right', 'east'
-    },
-}
+solved_places = {
+    "a1": {
+		"ZONENAME": "At the Jukebox",
+		"DESCRIPTION": "Your at the classic jukebox filled with some of the most classic hits",
+		"EXAMINATION": "You see a coin slot. it reads \"10 cents per song\" you see a forward and back button to select song",
+		"SOLVED": "False",
+		"UP": ["up", "north"],
+		"DOWN": "b1",
+		"LEFT": ["left", "west"],
+		"RIGHT": "a2"
+	},
+	"a2": {
+		"ZONENAME": "At the Booth",
+		"DESCRIPTION": "description",
+		"EXAMINATION": "examine",
+		"SOLVED": "False",
+		"UP": ["up", "north"],
+		"DOWN": ["down", "south"],
+		"LEFT": ["left", "west"],
+		"RIGHT": ["right", "east"]
+	},
+	"b1": {
+		"ZONENAME ": "Inside the Diner ",
+		"DESCRIPTION ": "This is the Jukebox diner",
+		"EXAMINATION ": "examine ",
+		"SOLVED": "False ",
+		"UP ": [
+			"up ",
+			"north "
+		],
+		"DOWN ": [
+			"down ",
+			"south "
+		],
+		"LEFT ": [
+			"left ",
+			"west "
+		],
+		"RIGHT ": [
+			"right ",
+			"east "
+		]
+	},
+	"b2 ": {
+		"ZONENAME ": "Outside the Jukebox Diner ",
+		"DESCRIPTION ": "description ",
+		"EXAMINATION ": "examine ",
+		"SOLVED ": "False",
+		"UP ": "a2 ",
+		"LEFT ": "b1 ",
+		"RIGHT ": [
+			"right ",
+			"east "
+		]
+            }
